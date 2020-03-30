7f5de9a01bafe1acb08d49a16bfd8d93bae9f903
KarlKobel <konemail35@gmail.com>
2020-03-29 19:30:44 -0500

    Add $defines for LED_ON & LED_OFF Add LedMask Add RedPattern & GreenPattern

--- V4/Src/LED.c
+++ V4/Src/LED.c
@@ -7,10 +7,19 @@
 
 #include "LED.h"
 
+#define LED_ON	GPIO_PIN_SET
+#define LED_OFF GPIO_PIN_RESET
+
+
 struct netif gnetif;
 
-static int LedBlink = 0;
+//static int LedBlink = 0;
+static uint32_t LedMask = 1;
 
+static uint32_t RedPattern;
+static uint32_t GreenPattern;
+
+#ifdef OLD
 void StatusLed(int s)
 {
 
@@ -21,42 +30,125 @@ void StatusLed(int s)
 	else if(s == LED_ON)
 	{
 		LedBlink = 0;
-		HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED, GPIO_PIN_SET);
 	}
 	else
 	{
 		LedBlink = 0;
-		HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED, GPIO_PIN_RESET);
 	}
 }
 
 
-void LedTask(void* argument)
+void LedTaskOld(void* argument)
 {
 
 	while(1)
 	{
-		HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(RED_LED_PORT, RED_LED, GPIO_PIN_SET);
 		if(LedBlink)
 		{
-			HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED, GPIO_PIN_RESET);
 		}
 		osDelay(pdMS_TO_TICKS(500));
-		HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(RED_LED_PORT, RED_LED, GPIO_PIN_RESET);
 		if(LedBlink)
 		{
-			HAL_GPIO_WritePin(LD1_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED, GPIO_PIN_SET);
 		}
 		osDelay(pdMS_TO_TICKS(500));
 
 
 		if(netif_is_link_up(&gnetif))
 		{
-			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(BLUE_LED_PORT, BLUE_LED, GPIO_PIN_SET);
 		}
 		else
 		{
-			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(BLUE_LED_PORT, BLUE_LED, GPIO_PIN_RESET);
 		}
 	}
 }
+#endif
+
+void HeartbeatLed(uint32_t nPattern)
+{
+	RedPattern = nPattern;
+	if(RedPattern == 0)
+	{
+		HAL_GPIO_WritePin(RED_LED_PORT, RED_LED, LED_OFF);
+	}
+}
+
+void StatusLed(uint32_t nPattern)
+{
+	GreenPattern = nPattern;
+	if(GreenPattern == 0)
+	{
+		HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED, LED_OFF);
+	}
+}
+
+
+
+void DoRedLed(void)
+{
+
+	if(RedPattern)
+	{
+		if((RedPattern & LedMask) != 0)
+		{
+			HAL_GPIO_WritePin(RED_LED_PORT, RED_LED, LED_ON);
+		}
+		else
+		{
+			HAL_GPIO_WritePin(RED_LED_PORT, RED_LED, LED_OFF);
+		}
+	}
+}
+
+void DoGreenLed(void)
+{
+
+	if(GreenPattern)
+	{
+		if((GreenPattern & LedMask) != 0)
+		{
+			HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED, LED_ON);
+		}
+		else
+		{
+			HAL_GPIO_WritePin(GREEN_LED_PORT, GREEN_LED, LED_OFF);
+		}
+	}
+}
+
+void LedTask(void* argument)
+{
+
+	while(1)
+	{
+		DoRedLed();
+		DoGreenLed();
+
+		LedMask <<= 1;
+		if(LedMask == 0)
+		{
+			LedMask = 1;
+		}
+
+		osDelay(pdMS_TO_TICKS(100));
+
+
+		if(netif_is_link_up(&gnetif))
+		{
+			HAL_GPIO_WritePin(BLUE_LED_PORT, BLUE_LED, LED_ON);
+		}
+		else
+		{
+			HAL_GPIO_WritePin(BLUE_LED_PORT, BLUE_LED, LED_OFF);
+		}
+	}
+}
+
+
