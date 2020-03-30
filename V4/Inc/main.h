aead5c55b0cb7ec110999064f7f5fe35c241e099
KarlKobel <konemail35@gmail.com>
2020-03-29 19:43:56 -0500

    Fix the LED 1 pin

--- V4/Inc/main.h
+++ V4/Inc/main.h
@@ -86,8 +86,8 @@ void Error_Handler(void);
 #define USB_PowerSwitchOn_GPIO_Port GPIOG
 #define USB_OverCurrent_Pin GPIO_PIN_7
 #define USB_OverCurrent_GPIO_Port GPIOG
-#define LD1_Pin GPIO_PIN_9
-#define LD1_GPIO_Port GPIOC
+#define LD1_Pin GPIO_PIN_0
+#define LD1_GPIO_Port GPIOB
 #define USB_SOF_Pin GPIO_PIN_8
 #define USB_SOF_GPIO_Port GPIOA
 #define USB_VBUS_Pin GPIO_PIN_9
