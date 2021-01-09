6f91ff67b10f4bb687c5cb9d0f42013c831bc251
KarlKobel <konemail35@gmail.com>
2021-01-08 22:05:48 -0600

    change the structure name (didn't match the current structure)

--- V4/Net Apps/tftp/vfs.c
+++ V4/Net Apps/tftp/vfs.c
@@ -46,7 +46,7 @@ int vfs_read (void* buffer, int dummy, int len, vfs_file_t* file) {
 vfs_dirent_t* vfs_readdir(vfs_dir_t* dir) {
 	FILINFO fi;
 #if _USE_LFN
-	fi.lfname = NULL;
+//k	fi.fname = NULL;
 #endif
 	FRESULT r = f_readdir(dir, &fi);
 	if (r != FR_OK) return NULL;
@@ -58,7 +58,7 @@ vfs_dirent_t* vfs_readdir(vfs_dir_t* dir) {
 int vfs_stat(vfs_t* vfs, const char* filename, vfs_stat_t* st) {
 	FILINFO f;
 #if _USE_LFN
-	f.lfname = NULL;
+//k	f.fname = NULL;
 #endif
 	if (FR_OK != f_stat(filename, &f)) {
 		return 1;
