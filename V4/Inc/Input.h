168ffb32a1d18336aaad87aa5064d188a78d13d9
KarlKobel <konemail35@gmail.com>
2021-08-24 19:07:56 -0500

    Add Input 6 (the ACK ststus)

--- V4/Inc/Input.h
+++ V4/Inc/Input.h
@@ -17,6 +17,7 @@ extern uint32_t GetInput2(void);
 extern uint32_t GetInput3(void);
 extern uint32_t GetInput4(void);
 extern uint32_t GetInput5(void);
+extern uint32_t GetInput6(void);
 
 extern uint32_t GetInputs(void);
 
