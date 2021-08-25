9937a3c6cf239d6e90f35bbdae10e02691cbb5ca
KarlKobel <konemail35@gmail.com>
2021-08-24 19:24:14 -0500

    add the ADC and DMA interrupt handlers

--- V4/Src/stm32f4xx_it.c
+++ V4/Src/stm32f4xx_it.c
@@ -27,6 +27,10 @@
 /* USER CODE BEGIN Includes */
 /* USER CODE END Includes */
 
+extern ADC_HandleTypeDef hadc1;
+
+extern DMA_HandleTypeDef hdma_adc1;
+
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN TD */
 
@@ -205,6 +209,30 @@ void OTG_FS_IRQHandler(void)
 }
 
 /* USER CODE BEGIN 1 */
+void ADC_IRQHandler(void)
+{
+  /* USER CODE BEGIN ADC_IRQn 0 */
+
+  /* USER CODE END ADC_IRQn 0 */
+  HAL_ADC_IRQHandler(&hadc1);
+  /* USER CODE BEGIN ADC_IRQn 1 */
+
+  /* USER CODE END ADC_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA2 stream0 global interrupt.
+  */
+void DMA2_Stream0_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA2_Stream0_IRQn 0 */
+
+  /* USER CODE END DMA2_Stream0_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_adc1);
+  /* USER CODE BEGIN DMA2_Stream0_IRQn 1 */
+
+  /* USER CODE END DMA2_Stream0_IRQn 1 */
+}
 
 /* USER CODE BEGIN 1 */
 
